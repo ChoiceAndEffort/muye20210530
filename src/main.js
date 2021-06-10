e89e4876fb62b43568e8227b52abcc0f508797f1
lei gong <291523450@qq.com>
2021-06-10 21:53:29 +0800

    feat:增加组件化按需引入

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import App from "./App.vue";
 import router from "./router";
 import "@/styles/reset.less";
 
-import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+import {
+  Icon,
+  Menu,
+  Submenu,
+  MenuItem,
+  Carousel,
+  CarouselItem,
+  Button,
+  Timeline,
+  TimelineItem,
+  Card,
+  Divider,
+} from "element-ui";
 
 import Header from "@/components/header.vue";
 import Footer from "@/components/footer.vue";
@@ -14,8 +26,17 @@ Vue.component("Header", Header);
 Vue.component("Footer", Footer);
 Vue.component("MuBan", MuBan);
 
-Vue.use(ElementUI);
-
+Vue.use(Icon);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(Carousel);
+Vue.use(CarouselItem);
+Vue.use(Button);
+Vue.use(Timeline);
+Vue.use(TimelineItem);
+Vue.use(MenuItem);
+Vue.use(Card);
+Vue.use(Divider);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/home/circle.vue
+++ src/views/home/circle.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="circle">
     <h3>企业介绍</h3>
-    <div class="content">
+    <div class="content" @click="handleRouterPush">
+      <p>集团化消费循环农业食材专供小镇</p>
       <!-- <img :src="circleEN" alt srcset /> -->
     </div>
   </div>
@@ -17,7 +18,9 @@ export default {
     };
   },
   methods: {
-    handAddMore() {},
+    handleRouterPush() {
+      this.$router.push({ name: "Introduce" });
+    },
   },
 };
 </script>
@@ -35,14 +38,45 @@ export default {
   .content {
     height: 400px;
     width: 100%;
-    background: url("~@/assets/images/circleEN.png") no-repeat 0 0;
-    // img {
-    //   height: 100%;
-    //   width: 100%;
-    // }
-    // img:hover{
+    background: url("~@/assets/images/circleEN.png") no-repeat left top/100%
+      100%;
 
-    // }
+    p {
+      color: #409eff;
+      font-size: 28px;
+      background: #fff;
+      width: 40%;
+      height: 50px;
+      line-height: 50px;
+      opacity: 0.6;
+    }
+  }
+  .content:hover {
+    p {
+      color: #ce1a1a;
+      opacity: 1;
+      position: relative;
+      animation: mymove 5s infinite;
+      -webkit-animation: mymove 5s infinite alternate; /*Safari and Chrome*/
+    }
+  }
+}
+
+@keyframes mymove {
+  0% {
+    left: 0%;
+  }
+  100% {
+    left: 60%;
+  }
+}
+
+@-webkit-keyframes mymove /*Safari and Chrome*/ {
+  0% {
+    left: 0%;
+  }
+  100% {
+    left: 60%;
   }
 }
 </style>
